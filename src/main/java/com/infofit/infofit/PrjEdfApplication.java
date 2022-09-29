b340749b05c7bb8f9823b87daceb479f06cfd0e3
Lucas Santos <lucassantosdev@hotmail.com>
2022-09-29 13:06:41 -0300

    FirstCommit

---
+++ src/main/java/com/infofit/infofit/PrjEdfApplication.java
@@ -0,0 +1,13 @@
+package com.infofit.infofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjEdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjEdfApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/infofit/infofit/model/Illness.java
@@ -0,0 +1,9 @@
+package com.infofit.infofit.model;
+
+public class Illness {
+	
+	private Long codeIllness;
+	private String nameIllness;
+	private String descriptionIllness;
+	
+}
